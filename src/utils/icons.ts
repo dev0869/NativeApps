bc263dac629b1f05dd22c0be8cf7d4a94708c71a
pankajkhuswaha <khuswahapankaj00@gmail.com>
2023-11-24 11:01:20 +0530

    commit

---
+++ src/utils/icons.ts
@@ -0,0 +1,6 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faSquareCheck } from "@fortawesome/free-solid-svg-icons/faSquareCheck";
+
+const icon = library.add(fab, faSquareCheck);
+export default icon;
