17a7e40898924973ed62011631e620820bf12a9f
dev0869 <deveshbisht36@gmail.com>
2023-11-24 12:28:29 +0530

    done

--- main.tsx
+++ main.tsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useState } from "react";
+import React, { useState } from "react";
 import EnterNumber from "./src/components/auth/EnterMobile";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
